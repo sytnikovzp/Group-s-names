406352747cb07891036b0219b46d2f053dc0cbbb
alex <sytnikov.zp@gmail.com>
2024-02-25 18:28:45 +0200

    Add regExp for Groups

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 'use strict';
 
+const arrGroups = [
+  'FE2021-5',
+  'Fe2020-2',
+  'FD2021',
+  'fD2022-1',
+  'FE2020-3',
+  'FD2020/2',
+  'fe2021-3',
+  'FD2020$5',
+  'FE2008-52',
+  'Fm2008-23',
+  'FM2022-1',
+];
 
+function checkGroupsNames(arrGroups) {
+  // const groupReg = /^[A-Z]{2}|[a-z]{2}20\d{2}(-\d+)?$/;
+  const groupReg = /^([A-Z]{2}|[a-z]{2})/;
+
+  arrGroups.forEach(function (group, i, arr) {
+    if(groupReg.test(group[i])){
+      console.log(`${group} - Success`);
+    }
+    console.log(`${group} - Error`);
+  });
+}
+
+checkGroupsNames(arrGroups);
